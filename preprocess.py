74fe6e2ed809cf544653b5fd772ac306a12c57a5
Eric Taseski <arvicz22@gmail.com>
2013-12-06 17:26:55 -0500

    Starting scikit learning preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,19 @@
 import csv
+from sklearn.feature_extraction.text import CountVectorizer
 
-with open('train.csv', 'rb') as tf,\
-        open('pre/input.csv', 'wb') as pi,\
-        open('pre/output.csv', 'wb') as po:
+with open('train.csv', 'rb') as tf:
     r = csv.reader(tf, delimiter=',')
-    wi = csv.writer(pi, delimiter=',')
-    wo = csv.writer(po, delimiter=',')
+    # wi = csv.writer(pi, delimiter=',')
+    # wo = csv.writer(po, delimiter=',')
 
     all_fields = list(r)
 
-    pre_input = map(lambda row: [row[1]], all_fields)
+    corpus = map(lambda row: [row[1]], all_fields)
     pre_output = map(lambda row: row[4:], all_fields)
 
-    for row_in, row_out in zip(pre_input, pre_output):
-        wi.writerow(row_in)
-        wo.writerow(row_out)
+    vectorizer = CountVectorizer(min_df=1)
+    X = vectorizer.fit_transform(corpus)
+
+    # for row_in, row_out in zip(pre_input, pre_output):
+    #     wi.writerow(row_in)
+    #     wo.writerow(row_out)
