cf933e4b6e9d190ce654908d49d8b2528357011a
Nand Dalal <nndalal@umich.edu>
2013-12-06 19:19:48 -0500

    Dimensionality reduction ftw

--- preprocess.py
+++ preprocess.py
@@ -13,9 +13,15 @@ with open('train.csv', 'rb') as tf:
 
     corpus = [c[0] for c in pre_input]
 
-    # vectorizer = CountVectorizer(min_df=1)
-    # X = vectorizer.fit_transform(corpus)
-
+    vectorizer = CountVectorizer(min_df=1600, max_df=64000)
+    X = vectorizer.fit_transform(corpus)
+    a=0
+    for v in vectorizer.get_feature_names():
+        if v=='the':
+            a = 1
+        print v
+    print len(vectorizer.get_feature_names())
+print a
     # for row_in, row_out in zip(pre_input, pre_output):
     #     wi.writerow(row_in)
     #     wo.writerow(row_out)
